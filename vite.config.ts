a84cd58c01ee9ec3972d7b5937802a2dc207edcd
colossusofNero <scottroelofs@icloud.com>
2025-08-28 22:17:49 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': '/src'
+      '@': path.resolve(__dirname, './src')
     }
   },
   build: {
-    outDir: 'dist',
+    commonjsOptions: {
+      transformMixedEsModules: true
+    },
     rollupOptions: {
       output: {
         manualChunks(id) {
